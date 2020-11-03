fb8cbbe97aaaa43c97cbaad6f8ad6826c054a189
90houlaoheshang <907333918@qq.com>
2020-11-03 10:28:20 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="StrDiffSynch",
-    version="2.0.1",
+    version="2.1.0",
     author="Antas",
     author_email="",
     description="One of two strings can change into the other when absorbing the difference among them. "
